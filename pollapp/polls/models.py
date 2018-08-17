c7a40bd29415b67e84b9e325f96b8cdf7f26596c
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2018-08-17 20:06:32 +0530

    complete admin, poll app

--- pollapp/polls/models.py
+++ pollapp/polls/models.py
@@ -1,5 +1,6 @@
 import json
-from datetime import datetime
+import datetime
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -11,6 +12,13 @@ class Question(models.Model):
 
     # def __str__(self):
     #     return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     @staticmethod
     def datetime_converter(o):
